51554da5b38a6f0e4661e893745e0c94d8150c9d
kunal1purohit <115582602+kunal1purohit@users.noreply.github.com>
2024-07-09 11:22:42 +0530

    login component - useform

--- mega-blog/src/comp/index.js
+++ mega-blog/src/comp/index.js
@@ -3,6 +3,8 @@ import Footer from "./footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import Logout from "./header/Logout";
+import Button from './Button'
+import Input from './Input'
 
 
-export {Header,Footer,Container,Logo,Logout};
+export {Header,Footer,Container,Logo,Logout,Button,Input};
